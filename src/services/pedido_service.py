36c28a532f476e67a864938a7135779b7f6e5467
Ícaro Yan Rosário da Silva <icaro.rosario@fieb.org.br>
2026-04-23 13:31:02 -0300

    Arquitetura em camdas: Refatoração da camada de serviços

--- src/services/pedido_service.py
+++ src/services/pedido_service.py
@@ -1,15 +1,18 @@
 from src.models.pedido import Pedido
+from src.repositories.pedido_repository import PedidoRepository
 
 class PedidoService:
     """Classe de serviço para processar pedidos e aplicar descontos."""
     
-    def __init__(self) -> None:
-        self.pedidos : list[Pedido] = []
+    def __init__(self, repository : PedidoRepository) -> None:
+        # self.pedidos : list[Pedido] = []
+        self.repository = repository
         
     def adicionar_pedido(self, pedido: Pedido):
-            self.pedidos.append(pedido)
+        self.repository.adicionar_pedido(pedido)
             
     def processar_pedidos(self):
+        pedidos = self.repository.listar_pedidos()
         for pedido in self.pedidos:
             print(f"Cliente: {pedido.cliente}")
             print(f"Valor final: {pedido.valor_final(pedido.valor_original)}")
